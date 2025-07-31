cd3bf9400f918a9f05b234aabd4c42a46e384e9d
Nazrul Islam <nazrulislam@Nazruls-MacBook.local>
2025-07-31 22:47:30 +0600

    Login signup database connected

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ const login = require('./config');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
@@ -18,6 +21,47 @@ app.get('/signup', (req, res) => {
     res.render('signup', { title: 'Register' });
 });
 
+app.post('/signup', async (req, res) => {
+    const data = {
+        email : req.body.email,
+        password : req.body.password,
+    }
+
+    const existingUser = await login.findOne({ email: data.email });
+    if (existingUser) {
+        return res.status(400).send('User already exists');
+    }else if(!existingUser){
+        const hashedPassword = await bcrypt.hash(data.password, 10);
+        data.password = hashedPassword;
+        const userdata = await login.insertMany(data);
+        console.log(userdata);
+
+    }
+
+
+});
+
+
+app.post('/login', async (req, res) => {
+    try{
+        const check = await login.findOne({ email: req.body.email });
+        if (check) {
+            const isMatch = await bcrypt.compare(req.body.password, check.password);
+            if (isMatch) {
+                res.status(200).send('Login successful');
+            } else {
+                res.status(400).send('Invalid password');
+            }
+        } else {
+            res.status(400).send('User not found');
+        }
+
+    }catch (error) {
+        console.error('Error during login:', error);
+        res.status(500).send('Internal server error');
+    }
+    
+});
 
 
 
